1bffbfb5ca24cf11773e80cf209697f08f2cbbbf
shreesh tiwari <shreesh@localhost.localdomain>
2025-05-26 12:25:02 -0400

    deleted that file

--- chat-app/chat-app-backend/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-// routes/user.routes.js
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/user.controller');
-
-router.get('/search', userController.searchUsers);
-router.get('/:id', userController.getUserById); 
-
-module.exports = router;
